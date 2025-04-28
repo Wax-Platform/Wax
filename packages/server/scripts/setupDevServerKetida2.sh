2ef0b2efefc857289253ecbf012d8370a98943c1
Yannis Barlas <yannisbarlas@gmail.com>
2025-04-28 20:21:32 +0300

    fix(server): fix pg extensions

--- packages/server/scripts/setupDevServerKetida2.sh
+++ packages/server/scripts/setupDevServerKetida2.sh
@@ -3,7 +3,7 @@ set -x
 # This is run through docker. Its CWD will be the root folder.
 sh scripts/wait-for-it db:5432 -t 60
 sh scripts/wait-for-it http://s3:9001 -t 60
-sh scripts/wait-for-it http://epubchecker:3001/healthcheck -t 60
+# sh scripts/wait-for-it http://epubchecker:3001/healthcheck -t 60
 sh scripts/wait-for-it http://pagedjs:3003/healthcheck -t 60
 sh scripts/wait-for-it http://xsweet:3004/healthcheck -t 60
 
