8d6eb425afbf52f8038d920125464503dfffeff3
Yannis Barlas <yannisbarlas@gmail.com>
2025-05-13 15:36:58 +0300

    fix(server): remove pgcrypto from migrations

--- packages/server/models/docTreeManager/migrations/1705493035-doc-tree-manager-migration.js
+++ packages/server/models/docTreeManager/migrations/1705493035-doc-tree-manager-migration.js
@@ -1,30 +1,25 @@
-exports.up = async (knex) => {
-  await knex.raw(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`);
-  await knex.schema.createTable('doc_tree_manager', (table) => {
+exports.up = async knex => {
+  await knex.schema.createTable('doc_tree_manager', table => {
     table
       .uuid('id')
       .primary()
       .notNullable()
-      .defaultTo(knex.raw('public.gen_random_uuid()'));
+      .defaultTo(knex.raw('public.gen_random_uuid()'))
     table
       .timestamp('created', { useTz: true })
       .notNullable()
-      .defaultTo(knex.fn.now());
-    table.timestamp('updated', { useTz: true });
-    table.text('title');
-    table.uuid('parent_id');
-    table.jsonb('children').notNullable();
-    table.boolean('is_folder').notNullable().defaultTo(false);
+      .defaultTo(knex.fn.now())
+    table.timestamp('updated', { useTz: true })
+    table.text('title')
+    table.uuid('parent_id')
+    table.jsonb('children').notNullable()
+    table.boolean('is_folder').notNullable().defaultTo(false)
 
     // foreign
-    table
-      .uuid('book_component_id')
-      .references('id')
-      .inTable('book_component');
-  });
-};
+    table.uuid('book_component_id').references('id').inTable('book_component')
+  })
+}
 
-exports.down = async (knex) => {
-  await knex.schema.dropTable('doc_tree_manager');
-};
-
+exports.down = async knex => {
+  await knex.schema.dropTable('doc_tree_manager')
+}
