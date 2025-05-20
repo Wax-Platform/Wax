6fbd346faa90a3185de5347a932b943e298c64ba
Giannis Kopanas <jkopanas@gmail.com>
2025-05-20 11:39:47 +0300

    fix ai designer

--- packages/client/app/pages/AiPDFDesigner.page.js
+++ packages/client/app/pages/AiPDFDesigner.page.js
@@ -6,81 +6,42 @@ import { useParams } from 'react-router-dom'
 import { GET_ENTIRE_BOOK, GET_BOOK_COMPONENT, GET_BOOKS } from '../graphql'
 import AiPDFDesigner from '../ui/AiPDFDesigner/AiPDFDesigner'
 import { CssAssistantContext } from '../ui/AiPDFDesigner/hooks/CssAssistantContext'
+import YjsContext from '../ui/provider-yjs/YjsProvider'
 
-const AiPDFDesignerPage = ({ bookId }) => {
+export default () => {
   const { bookComponentId } = useParams()
   const { setPassedContent } = useContext(CssAssistantContext)
-
+  const { ydoc } = useContext(YjsContext)
+  let content = ''
   const [bookTitle, setBookTitle] = useState('')
-
-  const { data: bookQueryData } = useQuery(GET_ENTIRE_BOOK, {
-    fetchPolicy: 'network-only',
-    nextFetchPolicy: 'network-only',
-    variables: {
-      id: bookId,
-    },
-  })
 
   const [getDivision] = useLazyQuery(GET_BOOK_COMPONENT, {
     fetchPolicy: 'network-only',
     nextFetchPolicy: 'network-only',
   })
 
-  useEffect(() => {
-    if (bookQueryData?.getBook?.divisions[1]) {
-      setBookTitle(bookQueryData?.getBook.title)
-      // templatesData && console.log(templatesData)
+  useEffect(()=> {
+    setPassedContent('')
+    getDivision({ variables: { id: bookComponentId } })
+      .then(({ data: chapter }) => {
+        if (ydoc) {
+          content = ydoc.getText('html').toString() 
+        } else {
+          content = chapter.getBookComponent.content
+        }
 
-      // const chaptersIds = bookQueryData.getBook.divisions[1].bookComponents.map(
-      //   division => division.id,
-      // )
+        setBookTitle(chapter.getBookComponent.title)
 
-      // console.log(chaptersIds)
-      const chaptersIds = [bookComponentId]
-
-      setPassedContent('')
-      Promise.all(
-        chaptersIds.map((chapterId, i) =>
-          getDivision({ variables: { id: chapterId } })
-            .then(
-              ({ data: chapter }) =>
-                `<div class="chapter chapter-${i + 1}">${
-                  chapter.getBookComponent.content
-                }</div>`,
-            )
-            .catch(e => {
-              throw new Error(e)
-            }),
-        ),
-      ).then(chaptersContent => {
-        setPassedContent(chaptersContent.join(''))
+        setPassedContent(
+          `<div class="chapter chapter-1">${content}</div>`
+        )
       })
-    }
-  }, [bookQueryData])
+      .catch(e => {
+        setPassedContent('')
+        throw new Error(e)
+      })
+  },[])
 
   return <AiPDFDesigner bookTitle={bookTitle} />
 }
 
-// eslint-disable-next-line react/function-component-definition
-export default props => {
-  const { loading, data: dataBooks } = useQuery(GET_BOOKS, {
-    fetchPolicy: 'network-only',
-    variables: {
-      options: {
-        archived: false,
-        orderBy: {
-          column: 'title',
-          order: 'asc',
-        },
-        page: 0,
-        pageSize: 10,
-      },
-    },
-  })
-
-  if (loading) return null
-
-  const [book] = dataBooks.getBooks.result
-
-  return <AiPDFDesignerPage {...props} bookId={book.id} />
-}
--- packages/client/app/pages/Exporter.page.js
+++ packages/client/app/pages/Exporter.page.js
@@ -697,8 +697,6 @@ const PreviewerPage = ({ bookId }) => {
     if ((target === 'pagedjs' || target === 'web') && !previewIsLoading) {
       if (ydoc) {
         const content = ydoc.getText('html').toString()
-
-        console.log(content)
         
         await updateContent({
           variables: {
