9ccf5e81b6a0fbb1b155bc2ad6192c7395fa172f
chris <kokosias@yahoo.gr>
2025-07-28 14:14:38 +0300

    add front graphql

---
+++ packages/client/app/graphql/chat.queries.js
@@ -0,0 +1,105 @@
+import { gql } from '@apollo/client'
+
+export const CREATE_CHAT_CHANNEL = gql`
+  mutation CreateChatChannel($input: CreateChatChannelInput!) {
+    createChatChannel(input: $input) {
+      id
+    }
+  }
+`
+
+export const GET_CHAT_CHANNEL = gql`
+  query chatChannel($id: ID!) {
+    chatChannel(id: $id) {
+      id
+      created
+      updated
+      chatType
+      relatedObjectId
+      messages {
+        id
+        content
+        created
+        user {
+          id
+          displayName
+        }
+        mentions
+        attachments {
+          name
+          url
+        }
+      }
+    }
+  }
+`
+
+export const FILTER_CHAT_CHANNELS = gql`
+  query ChatChannels($where: CreateChatChannelInput) {
+    chatChannels(where: $where) {
+      result {
+        id
+        created
+        updated
+        chatType
+        relatedObjectId
+        messages {
+          id
+          content
+          created
+          user {
+            id
+            displayName
+          }
+          mentions
+          attachments {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`
+
+export const SEND_MESSAGE = gql`
+  mutation SendMessage($input: SendChatMessageInput!) {
+    sendMessage(input: $input) {
+      id
+      chatChannelId
+      content
+      isDeleted
+      mentions
+      attachments {
+        name
+        url
+      }
+    }
+  }
+`
+
+export const MESSAGE_CREATED_SUBSCRIPTION = gql`
+  subscription MessageCreated($chatChannelId: ID!) {
+    messageCreated(chatChannelId: $chatChannelId) {
+      id
+      chatChannelId
+      content
+      created
+      user {
+        id
+        displayName
+      }
+      mentions
+      attachments {
+        name
+        url
+      }
+    }
+  }
+`
+
+export const CANCEL_EMAIL_NOTIFICATION = gql`
+  mutation cancelEmailNotification($chatChannelId: ID!) {
+    cancelEmailNotification(chatChannelId: $chatChannelId)
+  }
+`
